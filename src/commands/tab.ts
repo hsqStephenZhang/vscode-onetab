3feaa9c5e77d371b352d2912a97e159ca9bdd9fc
hsqStephenZhang <stephenzhang666666@gmail.com>
2024-04-19 15:04:31 +0800

    fix ts undefined error

--- src/commands/tab.ts
+++ src/commands/tab.ts
@@ -8,45 +8,47 @@ import { Global } from "../common/global";
 import { TabItem } from "../model/main/tabitem";
 
 export async function tabRestore(tab: TabItem) {
+  if (!tab.parentId) {
+    return;
+  }
   let groupId = tab.parentId;
 
-  if (groupId) {
-    const state = Global.tabsProvider.getState();
+  const state = Global.tabsProvider.getState();
 
-    let g = state.getGroup(groupId);
+  let g = state.getGroup(groupId);
 
-    if (g) {
-      if (g.isPinned()) {
-        vscode.window.showTextDocument(tab.fileUri, { preview: false });
-        Global.tabsProvider.refresh();
-      } else {
-        vscode.window.showTextDocument(tab.fileUri, { preview: false });
-        Global.tabsProvider.updateState((state) => {
-          state.removeTabFromGroup(groupId, tab.fileUri.fsPath);
-        })
-      }
+  if (g) {
+    if (g.isPinned()) {
+      vscode.window.showTextDocument(tab.fileUri, { preview: false });
+      Global.tabsProvider.refresh();
+    } else {
+      vscode.window.showTextDocument(tab.fileUri, { preview: false });
+      Global.tabsProvider.updateState((state) => {
+        state.removeTabFromGroup(groupId, tab.fileUri.fsPath);
+      })
     }
   }
 }
 
 export async function tabRemove(tab: TabItem) {
+  if (!tab.parentId) {
+    return;
+  }
   let groupId = tab.parentId;
 
-  if (groupId) {
-    const state = Global.tabsProvider.getState();
+  const state = Global.tabsProvider.getState();
 
-    let g = state.getGroup(groupId);
+  let g = state.getGroup(groupId);
 
-    if (g) {
-      if (g.isPinned()) {
-        vscode.window.showWarningMessage(
-          "this tab has been pinned within a group, please unpin the group before removing this tab"
-        );
-      } else {
-        Global.tabsProvider.updateState((state) => {
-          state.removeTabFromGroup(groupId, tab.fileUri.fsPath);
-        })
-      }
+  if (g) {
+    if (g.isPinned()) {
+      vscode.window.showWarningMessage(
+        "this tab has been pinned within a group, please unpin the group before removing this tab"
+      );
+    } else {
+      Global.tabsProvider.updateState((state) => {
+        state.removeTabFromGroup(groupId, tab.fileUri.fsPath);
+      })
     }
   }
 }
